90227601737e80996eb8397bd24e3ed1a78f68db
llouis <llouis@hfmi.com>
2020-01-21 09:30:33 -0400

    Remove my fire base creadentials

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,13 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
-import 'firebase/storage';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDHI5S7VvyYwtqG7Xn38PSOK9FZunIBV9s",
-    authDomain: "slackchat-3de48.firebaseapp.com",
-    databaseURL: "https://slackchat-3de48.firebaseio.com",
-    projectId: "slackchat-3de48",
-    storageBucket: "slackchat-3de48.appspot.com",
-    messagingSenderId: "1026623506542",
-    appId: "1:1026623506542:web:271ab6dde0095914"
-  };
+  // Please add your firebase credentials
+};
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  export default firebase;
+export default firebase;
