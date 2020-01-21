d74116f96b94a7092640b90732b3fac51f5d21c8
llouis <llouis@hfmi.com>
2020-01-21 09:21:28 -0400

    Remove un used variables

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,82 +1,82 @@
-import * as actionTypes from '../actions/types';
-import { combineReducers } from 'redux'
+import * as actionTypes from "../actions/types";
+import { combineReducers } from "redux";
 const initialUserState = {
-    currentUser: null,
-    isLoading: true
+  currentUser: null,
+  isLoading: true
 };
 
 const user_reducer = (state = initialUserState, action) => {
-    switch(action.type) {
-        case actionTypes.SET_USER:
-            return{
-                ...state,
-                currentUser: action.payload.currentUser,
-                isLoading: false
-            }
-        case actionTypes.CLEAR_USER:
-            return {
-                ...initialUserState,
-                isLoading: false
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case actionTypes.SET_USER:
+      return {
+        ...state,
+        currentUser: action.payload.currentUser,
+        isLoading: false
+      };
+    case actionTypes.CLEAR_USER:
+      return {
+        ...initialUserState,
+        isLoading: false
+      };
+    default:
+      return state;
+  }
+};
 
 const initialChannelState = {
-    currentChannel: null,
-    isPrivateChannel: false,
-    userPosts: null,
-}
+  currentChannel: null,
+  isPrivateChannel: false,
+  userPosts: null
+};
 
 const channel_reducer = (state = initialChannelState, action) => {
-    switch(action.type) {
-        case actionTypes.SET_CURRENT_CHANNEL:
-            return {
-                ...state,
-                currentChannel: action.payload.currentChannel
-            }
-        case actionTypes.SET_PRIVATE_CHANNEL:
-            return {
-                ...state,
-                isPrivateChannel: action.payload.isPrivateChannel
-            }
-        case actionTypes .SET_USER_POSTS:
-            return {
-                ...state,
-                userPosts: action.payload.userPosts             
-            }       
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case actionTypes.SET_CURRENT_CHANNEL:
+      return {
+        ...state,
+        currentChannel: action.payload.currentChannel
+      };
+    case actionTypes.SET_PRIVATE_CHANNEL:
+      return {
+        ...state,
+        isPrivateChannel: action.payload.isPrivateChannel
+      };
+    case actionTypes.SET_USER_POSTS:
+      return {
+        ...state,
+        userPosts: action.payload.userPosts
+      };
+    default:
+      return state;
+  }
+};
 
 const initialColorState = {
-    primary: {
-        hex:'#4c3c4c'
-    }, 
-    secondary: {
-        hex: '#eee'
-    }
+  primary: {
+    hex: "#4c3c4c"
+  },
+  secondary: {
+    hex: "#eee"
+  }
 };
 
 const color_reducer = (state = initialColorState, action) => {
-    switch(action.type) {
-        case actionTypes.SET_COLORS:
-            return {
-                ...state,
-                primary: action.payload.primary,
-                secondary: action.payload.secondary,
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case actionTypes.SET_COLORS:
+      return {
+        ...state,
+        primary: action.payload.primary,
+        secondary: action.payload.secondary
+      };
+    default:
+      return state;
+  }
+};
 
 const rootReducer = combineReducers({
-    user: user_reducer,
-    channel: channel_reducer,
-    colors: color_reducer
+  user: user_reducer,
+  channel: channel_reducer,
+  colors: color_reducer
 });
 
 export default rootReducer;
